10e404b066fa26226783056702a582a33575c6fe
Bitwise <93211307+jaackkiscool@users.noreply.github.com>
2022-05-06 16:08:21 +0300

    Update Lua.lua

--- Lua.lua
+++ Lua.lua
@@ -1,6 +1,32 @@
 local old_env = _ENV
 local bkey = {
 -- Blacklisted constants, if you want you could edit them.
+    "getfenv",
+    "string",
+    "bit32",
+    "pairs",
+    "select",
+    "math",
+    "rawset",
+    "rawequal",
+    "rawget",
+    "pairs",
+    "next",
+    "setfenv",
+    "error",
+    "assert",
+    "pcall",
+    "getmetatable",
+    "debug",
+    "unpack",
+    "setmetatable",
+    "tonumber",
+    "math",
+    "type",
+    "bit32",
+    "table",
+    "tostring",
+    "bit"
 }
 _ENV = {["old_env"]=old_env,["bkey"]=bkey}
 
